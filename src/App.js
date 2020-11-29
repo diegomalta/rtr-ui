1fa0b12946701029f3c95d054112b1595893fd06
Diego Malta <diego_malta@hotmail.com>
2020-11-29 17:11:54 -0600

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/EmailForm';
+import EmailForm from './components/EmailForm';
+
+function App() {
+  return (
+      <EmailForm />
+  );
+}
+
+export default App;
---
+++ src/components/EmailForm.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function EmailForm() {
+  return (
+    <div>
+      <h1>Hola Mundo!</h1>
+    </div>
+  );
+}
+
+export default EmailForm;
